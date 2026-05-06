305db09dd2d605a2c71191d68d654e3470eca64f
Nguyễn Hữu Tú <huutu2503@gmail.com>
2026-05-06 18:03:38 +0700

    lay ra danh sach bai viet

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,15 @@
+import Articles from "./models/article.model";
+
 export const resolvers = {
   Query: {
     hello: () => {
       return "Hello word!";
+    },
+    getListArticles: async () => {
+      const articles = await Articles.find({
+        deleted: false,
+      });
+      return articles;
     },
   },
 };
